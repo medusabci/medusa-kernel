2513bb65359cdffe1c77290c7588ce4c5f1670bb
dmarcos97 <diego.marcos@gib.tel.uva.es>
2022-06-09 12:58:59 +0200

    Fast LZ added

--- medusa/local_activation/nonlinear_parameters.py
+++ medusa/local_activation/nonlinear_parameters.py
@@ -5,6 +5,7 @@ from scipy.spatial.distance import pdist
 from scipy.signal import decimate
 import math
 from medusa.components import ThreadWithReturnValue
+import ctypes
 
 # PARAMETERS
 ctm_r = 0.1
@@ -160,6 +161,7 @@ def sample_entropy(signal, m, r, dist_type='chebyshev'):
 
     return value
 
+
 def multiscale_entropy(signal, max_scale, m, r):
     """ This method implements the Multiscale Entropy (MSE). MSE is a measurement of
     complexity which measures the irregularity of a signal over multiple time scales.
@@ -262,6 +264,48 @@ def lempelziv(signal):
         for index, thread in enumerate(working_threads):
             lz_channel_values[index] = thread.join()
         return lz_channel_values
+
+def lempelziv_fast(signal):
+    """
+       Calculate the  signal binarisation and the Lempel-Ziv's complexity. This
+       version allows the algorithm to be used for signals with more than one
+       channel at the same time. Faster as it is implemented in C
+
+       Parameters
+       ---------
+       signal: numpy 2D array
+           Signal with shape [n_samples, n_channels]
+
+       Returns
+       -------
+       lz_channel_values: numpy 1D matrix
+           Lempel-Ziv values for each channel.
+           [n_channels].
+    """
+    # Adapt the signal to the format required by the DLL
+    n_sample = signal.shape[0]
+    n_cha = signal.shape[1]
+    signal = signal.T.reshape(-1)
+
+    # Get function from dll
+    dll_file = ".\computeLZC.dll"
+    lib = ctypes.cdll.LoadLibrary(dll_file)
+    lzc_func = lib.computeLempelZivCmplx  # Access function
+
+    # Create empty output vector
+    lz_channel_values = np.zeros(int(n_cha), dtype=np.double)
+
+    # Define inputs and outputs
+    lzc_func.restype = None
+    array_1d_double = np.ctypeslib.ndpointer(dtype=np.double, ndim=1,
+                                             flags='CONTIGUOUS')
+    lzc_func.argtypes = [array_1d_double, array_1d_double, ctypes.c_int32,
+                         ctypes.c_int32]
+
+    # Call the function in the dll
+    lzc_func(signal, lz_channel_values, int(n_sample*n_cha), int(n_sample))
+
+    return lz_channel_values
 
 def multiscale_lempelziv(signal, W):
     """
@@ -526,7 +570,9 @@ def compute_nonlinear_metric(signal, epoch_len, param):
             param_values[ii, :] = multiscale_entropy(
                 signal_epoched[ii, :, :], mse_max_scale, sampen_m, sampen_r)
         elif param == 'LZC':
-            param_values[ii, :] = lempelziv(
+            # param_values[ii, :] = lempelziv(
+            #     np.squeeze(signal_epoched[ii, :, :]))
+            param_values[ii, :] = lempelziv_fast(
                 np.squeeze(signal_epoched[ii, :, :]))
         elif param == 'MultiscaleLZC':
             param_values[ii, :, :] = multiscale_lempelziv(
