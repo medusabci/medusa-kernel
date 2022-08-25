e67f3331be6390ebd80a98c15f84c55c2f989e6d
dmarcos97 <diego.marcos@gib.tel.uva.es>
2022-08-25 17:21:24 +0200

    Now a color map can be chosen

--- medusa/plots/brain_plots.py
+++ medusa/plots/brain_plots.py
@@ -1,3 +1,8 @@
+"""
+Created on Mon Mar 21 15:34:18 2022
+
+@author: Diego Marcos-Martínez
+"""
 # Python modules
 import argparse
 import os
@@ -11,6 +16,7 @@ from vispy.io import imread, load_data_file, read_mesh
 from vispy.scene.visuals import Mesh, Markers, Text, Graph
 from vispy.scene import transforms
 from vispy.visuals.filters import TextureFilter
+from vispy.color import get_colormap
 from medusa.meeg import EEGChannelSet
 
 
@@ -38,6 +44,7 @@ class TridimentionalBrain():
         self.connections_coords = None
         self.connections_values = None
         self.lines = None
+        self.lines_cmap = None
         self.n_subplots = None
 
         self.__check_subplot()
@@ -60,6 +67,7 @@ class TridimentionalBrain():
         self.canvas.show()
 
     def __check_subplot(self):
+        """ This function ensures subplots parameter is correctly defined"""
         try:
             if self.subplots is not None:
                 assert isinstance(self.subplots, tuple)
@@ -68,8 +76,8 @@ class TridimentionalBrain():
             else:
                 self.n_subplots = 1
                 self.subplots = (1, 1)
-        except Exception as ex:
-            print(ex)
+        except ValueError:
+            print("Subplots must be two-dimensional tuple or None")
 
     def __set_alpha(self):
         try:
@@ -91,6 +99,7 @@ class TridimentionalBrain():
             print(ex)
 
     def __set_cameras(self):
+        # TODO Eliminar esta función y poner fija ese tipo de camara
         try:
             self.cameras = []
             for i in range(self.n_subplots):
@@ -100,8 +109,9 @@ class TridimentionalBrain():
 
     def __set_canvas(self):
         try:
-            self.canvas = scene.SceneCanvas(keys='interactive', bgcolor=self.bg_color,
-                                            size=(850, 850),show=True)
+            self.canvas = scene.SceneCanvas(keys='interactive',
+                                            bgcolor=self.bg_color,
+                                            size=(850, 850), show=True)
             if self.cameras is not None:
                 assert len(self.cameras) == self.n_subplots
             else:
@@ -123,9 +133,11 @@ class TridimentionalBrain():
                         else:
                             if self.names is not None:
                                 assert isinstance(self.names[_view_idx], str)
-                                title = scene.Label(self.names[_view_idx], color=self.text_color)
+                                title = scene.Label(self.names[_view_idx],
+                                                    color=self.text_color)
                                 title.height_max = 60
-                                self.grid.add_widget(title, row=row, col=col, col_span=1)
+                                self.grid.add_widget(title, row=row, col=col,
+                                                     col_span=1)
                                 view = self.grid.add_view(row=row + 1, col=col)
                             else:
                                 view = self.grid.add_view(row=row, col=col)
@@ -145,24 +157,27 @@ class TridimentionalBrain():
             shading = None if args.shading == 'none' else args.shading
 
             for i in range(self.n_subplots):
-                path = os.path.join(os.path.dirname(__file__), 'templates/{}.npz'.format(self.models[i]))
+                path = os.path.join(os.path.dirname(__file__),
+                                    'templates/{}.npz'.format(self.models[i]))
                 model = np.load(path)
                 vertices, faces = model.f.vertices, model.f.faces
 
-                # TODO This step is temporary
+                # This is temporary, just to adjust shape and coordinates
                 vertices[:, 0] = 0.85 * vertices[:, 0] / np.max(vertices[:, 2])
                 vertices[:, 1] = 0.85 * vertices[:, 1] / np.max(vertices[:, 2])
                 vertices[:, 2] = 0.85 * vertices[:, 2] / np.max(vertices[:, 2])
 
                 # Adapt the depth to the scale of the mesh to avoid rendering artefacts.
-                self.views[i].camera.depth_value = 10 * (vertices.max() - vertices.min())
+                self.views[i].camera.depth_value = 10 * (
+                            vertices.max() - vertices.min())
 
                 # Define transparency of each brain
                 if self.translucent[i]:
                     alpha = 0.3
                 else:
                     alpha = 1
-                mesh = Mesh(vertices, faces, shading=shading, color=(1, 1, 1, alpha))
+                mesh = Mesh(vertices, faces, shading=shading,
+                            color=(1, 1, 1, alpha))
                 mesh.shading_filter.shininess = 1e+2
                 self.brain_visuals.append(mesh)
                 self.attach_headlight(mesh, self.views[i], self.canvas)
@@ -177,12 +192,15 @@ class TridimentionalBrain():
             print(ex)
 
     def attach_headlight(self, mesh, view, canvas):
+        """This function sets the initial light direction """
         light_dir = (1, 0, 1, 1)
         mesh.shading_filter.light_dir = light_dir[:3]
         initial_light_dir = view.camera.transform.imap(light_dir)
 
         @view.scene.transform.changed.connect
         def on_transform_change(event):
+            """ This function calculates the illumination every
+            time the brain is rotated"""
             transform = view.camera.transform
             mesh.shading_filter.light_dir = transform.map(initial_light_dir)[:3]
 
@@ -190,7 +208,7 @@ class TridimentionalBrain():
         try:
             if self.markers is None:
                 self.__init_markers()
-            markers = Markers()
+            markers = Markers(light_color=self.text_color)
             markers.set_data(locs)
             self.markers[sub_plot[0]][sub_plot[1]] = markers
 
@@ -199,16 +217,16 @@ class TridimentionalBrain():
         except Exception as ex:
             print(ex)
 
-    def set_labels(self, labels,locs, sub_plot):
+    def set_labels(self, labels, locs, sub_plot):
         try:
             if self.labels_text is None:
                 self.__init_labels()
 
             # Move the labels over the channels
             text_coord = locs.copy()
-            text_coord[:,2] += 0.1
-            labels_text = Text(labels,color = self.text_color,bold=True,
-                               pos = text_coord,font_size=50)
+            text_coord[:, 2] += 0.1
+            labels_text = Text(labels, color=self.text_color, bold=True,
+                               pos=text_coord, font_size=50)
             self.labels_text[sub_plot[0]][sub_plot[1]] = labels_text
 
             _view_idx = self.__calculate_subplot_idx(sub_plot)
@@ -216,35 +234,44 @@ class TridimentionalBrain():
         except Exception as ex:
             print(ex)
 
-    def __set_conn_color(self,sub_plot,threshold):
+    def __set_conn_color(self, sub_plot, threshold):
         try:
             # Mask under threshold values
-            # TODO Arreglar tema de colores/valores
-            mask = np.ones(len(self.connections_values[sub_plot[0]][sub_plot[1]]), dtype=bool)
-            mask[np.where(self.connections_values[sub_plot[0]][sub_plot[1]] < threshold)] = 0
+            mask = np.ones(
+                len(self.connections_values[sub_plot[0]][sub_plot[1]]),
+                dtype=bool)
+            mask[np.where(np.abs(self.connections_values[sub_plot[0]][
+                                     sub_plot[1]]) < threshold)] = 0
 
-            self.connections_values[sub_plot[0]][sub_plot[1]] = self.connections_values[sub_plot[0]][sub_plot[1]][mask]
+            # Delete under threshold values and their connection coordinates
+            self.connections_values[sub_plot[0]][sub_plot[1]] = \
+                self.connections_values[sub_plot[0]][sub_plot[1]][mask]
             self.connections_coords_mutable[sub_plot[0]][sub_plot[1]] = \
-            self.connections_coords_invariant[sub_plot[0]][sub_plot[1]][mask]
+                self.connections_coords_invariant[sub_plot[0]][sub_plot[1]][
+                    mask]
 
-            self.connections_values[sub_plot[0]][sub_plot[1]] = np.tile(self.connections_values[sub_plot[0]]
-                                                                        [sub_plot[1]], (2, 1)).reshape(
-                2 * len(self.connections_values[
-                            sub_plot[0]][sub_plot[1]]), order='F')
-            color = np.zeros((len(self.connections_values[sub_plot[0]][sub_plot[1]]), 4), dtype=np.float32)
-            color[:, 0] = self.connections_values[sub_plot[0]][sub_plot[1]]
-            color[:, -1] = 1
+            # Duplicate the matrix of values so that each point of the
+            # connection has the same value.
+            self.connections_values[sub_plot[0]][sub_plot[1]] = \
+                np.tile(self.connections_values[sub_plot[0]][sub_plot[1]],
+                        (2, 1)).reshape(2 * len(self.connections_values[
+                                                    sub_plot[0]][sub_plot[1]]),
+                                        order='F')
+            color = self.lines_cmap.map(self.connections_values[sub_plot[0]]
+                                        [sub_plot[1]])
             return color
         except Exception as ex:
             print(ex)
 
     def set_connections(self, adj_mat, locs, sub_plot=None, threshold=0.5,
-                        plot_markers=True, labels=None, plot_labels=False):
+                        plot_markers=True, labels=None, plot_labels=False,
+                        cmap='seismic'):
         try:
             if sub_plot is None and self.n_subplots == 1:
                 sub_plot = (0, 0)
             assert isinstance(sub_plot, tuple)
-            assert sub_plot[0] <= self.subplots[0] and sub_plot[1] <= self.subplots[1]
+            assert sub_plot[0] <= self.subplots[0] and sub_plot[1] <= \
+                   self.subplots[1]
 
             if plot_markers:
                 self.set_markers(locs, sub_plot)
@@ -257,50 +284,61 @@ class TridimentionalBrain():
                     self.set_labels(labels, locs, sub_plot)
 
             # Extract connectivity values
-            self.connections_values[sub_plot[0]][sub_plot[1]] = self.__extract_conn_values(adj_mat)
+            self.connections_values[sub_plot[0]][
+                sub_plot[1]] = self.__extract_conn_values(adj_mat)
 
             # Set connections matrix
-            self.connections_coords_invariant[sub_plot[0]][sub_plot[1]] = self.__set_connections_coords(locs,
-                                                                                                        self.connections_values
-                                                                                                        [sub_plot[0]][
-                                                                                                            sub_plot[
-                                                                                                                1]])
+            self.connections_coords_invariant[sub_plot[0]][
+                sub_plot[1]] = self.__set_connections_coords(locs,
+                                                             self.connections_values
+                                                             [sub_plot[0]][
+                                                                 sub_plot[
+                                                                     1]])
 
-            color = self.__set_conn_color(sub_plot,threshold)
+            # Get color map and color connections
+            self.lines_cmap = get_colormap(cmap)
+            color = self.__set_conn_color(sub_plot, threshold)
 
-            # Draw line conecctions
+            # Draw line connections
             if self.subplots is None:
                 _view_idx = 0
             else:
                 _view_idx = self.__calculate_subplot_idx(sub_plot)
-            self.lines[sub_plot[0]][sub_plot[1]] = scene.Line(antialias=True, parent = self.views[_view_idx].scene)
-            self.lines[sub_plot[0]][sub_plot[1]].set_data( pos=self.connections_coords_mutable[sub_plot[0]][sub_plot[1]],
-                color=color,  width=3,)
+            self.lines[sub_plot[0]][sub_plot[1]] = scene.Line(antialias=True,
+                                                              parent=self.views[
+                                                                  _view_idx].scene,
+                                                              )
+            self.lines[sub_plot[0]][sub_plot[1]].set_data(
+                pos=self.connections_coords_mutable[sub_plot[0]][sub_plot[1]],
+                color=color, width=4)
         except Exception as ex:
             print(ex)
 
-    def update_connections(self,adj_mat,sub_plot,threshold):
+    def update_connections(self, adj_mat, sub_plot, threshold):
         try:
             if sub_plot is None:
-                sub_plot = (0,0)
+                sub_plot = (0, 0)
             if threshold is None:
                 threshold = 0.5
             # Extract connectivity values
-            self.connections_values[sub_plot[0]][sub_plot[1]] = self.__extract_conn_values(adj_mat)
-            color = self.__set_conn_color(sub_plot,threshold)
+            self.connections_values[sub_plot[0]][
+                sub_plot[1]] = self.__extract_conn_values(adj_mat)
+            color = self.__set_conn_color(sub_plot, threshold)
             _view_idx = self.__calculate_subplot_idx(sub_plot)
 
             self.lines[sub_plot[0]][sub_plot[1]].parent = None
 
-            self.lines[sub_plot[0]][sub_plot[1]] = scene.Line(antialias=True, parent=self.views[_view_idx].scene)
-            self.lines[sub_plot[0]][sub_plot[1]].set_data(pos=self.connections_coords_mutable[sub_plot[0]][sub_plot[1]],
-                                                          color=color, width=3, )
+            self.lines[sub_plot[0]][sub_plot[1]] = scene.Line(antialias=True,
+                                                              parent=self.views[
+                                                                  _view_idx].scene)
+            self.lines[sub_plot[0]][sub_plot[1]].set_data(
+                pos=self.connections_coords_mutable[sub_plot[0]][sub_plot[1]],
+                color=color, width=4, )
             self.canvas.update()
         except Exception as ex:
             print(ex)
 
-
-    def __calculate_subplot_idx(self,sub_plot):
+    def __calculate_subplot_idx(self, sub_plot):
         try:
             _view_idx = sub_plot[1] + self.subplots[1] * sub_plot[0]
             return _view_idx
@@ -312,8 +350,10 @@ class TridimentionalBrain():
             subplots = self.subplots
             if self.n_subplots is None:
                 subplots = (1, 1)
-            self.connections_coords_invariant = np.empty(shape=subplots + (0,)).tolist()
-            self.connections_coords_mutable = np.empty(shape=subplots + (0,)).tolist()
+            self.connections_coords_invariant = np.empty(
+                shape=subplots + (0,)).tolist()
+            self.connections_coords_mutable = np.empty(
+                shape=subplots + (0,)).tolist()
             self.connections_values = np.empty(shape=subplots + (0,)).tolist()
             self.lines = np.empty(shape=subplots + (0,)).tolist()
         except Exception as ex:
@@ -361,22 +401,27 @@ class TridimentionalBrain():
 
 
 if __name__ == '__main__':
-    from vispy import app
+    from vispy.app import use_app
 
+    app = use_app("pyqt5")
+    app.create()
     # Set canvas
-    triplot = TridimentionalBrain(subplots=(2,1))
+    triplot = TridimentionalBrain( bg_color='white',
+                                  text_color='black', models=['B1_2'],translucent=[True])
 
     # Define channel set and its coord
     channel_set = EEGChannelSet(dim='3D', coord_system='cartesian')
     channel_set.set_standard_montage(standard='10-20')
     channel_coord = np.zeros((len(channel_set.channels), 3))
     for ch_idx, channel in enumerate(channel_set.channels):
-        channel_coord[ch_idx, 0] = channel['x']
-        channel_coord[ch_idx, 1] = channel['y'] - 0.2
-        channel_coord[ch_idx, 2] = channel['z']
-    adj_mat = np.random.random((len(channel_set.channels), len(channel_set.channels)))
-    triplot.set_connections(adj_mat, channel_coord, threshold=[0.9], sub_plot=(0, 0), plot_labels=True,labels=channel_set.l_cha)
-    triplot.set_connections(adj_mat, channel_coord, threshold=[0.4], sub_plot=(0, 1), plot_labels=True,labels=channel_set.l_cha)
+        channel_coord[ch_idx, 0] = 0.8 * (channel['x'])
+        channel_coord[ch_idx, 1] = 0.9 * (channel['y'] - 0.2)
+        channel_coord[ch_idx, 2] = 0.85 * (channel['z'])
+    adj_mat = np.random.randn(len(channel_set.channels),
+                              len(channel_set.channels))
+    adj_mat = 2 * adj_mat - 1
+    triplot.set_connections(adj_mat, channel_coord, threshold=[0.6], plot_labels=True,
+                            labels=channel_set.l_cha, plot_markers=True,cmap='Spectral')
     triplot.add_brains()
     triplot.canvas.show()
     app.run()
