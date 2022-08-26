aeb69fb00727cd704045d44700901c9cc42e4981
dmarcos97 <diego.marcos@gib.tel.uva.es>
2022-08-26 11:12:36 +0200

    Minor fixes

--- medusa/connectivity/phase_connectivity.py
+++ medusa/connectivity/phase_connectivity.py
@@ -3,6 +3,7 @@ import numpy as np
 from medusa import transforms
 from numba import jit
 import warnings
+import tensorflow as tf
 
 
 @jit(nopython=True, cache=True, parallel=True)
@@ -327,5 +328,5 @@ def phase_connectivity(data):
         # plv, pli, wpli, = __phase_connectivity_numba(data)
         plv, pli, wpli, = __phase_connectivity_cpu(data)
     # Remove nan values in wpli main diagonal
-    np.fill_diagonal(wpli,1)
+    wpli = tf.linalg.set_diag(wpli,np.ones(wpli.shape[0]))
     return plv, pli, wpli,
