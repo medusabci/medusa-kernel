bc20dfbedecabf05ffda2ebf043853512c29fe06
Eduardo Santamaría-Vázquez <edusan1213@gmail.com>
2022-09-08 18:20:55 +0200

    EMG signal added

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='medusa-kernel',
     packages=find_packages(),
-    version='0.3.2',
+    version='0.4.0',
     keywords=['Signal', 'Biosignal', 'EEG', 'BCI'],
     url='https://medusabci.com/',
     author='Eduardo Santamaría-Vázquez, '
