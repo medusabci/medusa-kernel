7a86bd009a597b75d6cad2977a91b4e2718b90e0
Eduardo Santamaría-Vázquez <edusan1213@gmail.com>
2022-09-29 19:48:01 +0200

    Testing branch rules

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
+# Just testing
+
 # read the contents of your README file
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
