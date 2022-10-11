83b0f828a9c41f86e1624daf4b86f3cb9945bc7a
dmarcos97 <diego.marcos@gib.tel.uva.es>
2022-10-11 13:57:58 +0200

    Script for utils functions. Now it only includes check_dimensions function to standardize data dimensions ([n_epochs,n_samples, n_channels])

---
+++ medusa/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def check_dimensions(data):
+    """This function checks if data dimensions are
+    [n_epochs, n_samples, n_channels] and transforms to it if not"""
+
+    # If dimension is samples
+    if len(data.shape) == 1:
+        return data[np.newaxis,:,np.newaxis]
+    # If dimensions are samples and channels
+    if len(data.shape) == 2:
+        return data[np.newaxis,:,np.newaxis]
