3613446a78b8abe265abf2ed3502b6ad850dde96
dmarcos97 <diego.marcos@gib.tel.uva.es>
2022-11-08 17:27:31 +0100

    AEC orthogonalized functions (both using CPU and GPU) have been vectorized and parallelized achieving a significant improvement in its performance.

--- medusa/connectivity/amplitude_connectivity.py
+++ medusa/connectivity/amplitude_connectivity.py
@@ -1,9 +1,25 @@
+"""
+In this module you will find useful functions to apply optimized amplitude-based
+connectivity metrics. Enjoy!
+
+@authors: Víctor Rodríguez-González and Diego Marcos-Martínez
+"""
+
+# Built-in imports
+import warnings
+
+# External imports
 import numpy as np
 from scipy import stats as sp_stats
+import tensorflow as tf
+from tensorflow_probability import stats as tfp_stats
+
+# Medusa imports
+import medusa.components
 from medusa import signal_orthogonalization as orthogonalizate
 from medusa.transforms import hilbert
 from medusa import pearson_corr_matrix as corr
-import warnings
+from medusa.utils import check_dimensions
 
 
 def __aec_gpu(data):
@@ -93,81 +109,50 @@ def __aec_ort_gpu(data):
     envelope correlation using GPU. This orthogonalized version minimizes the
     spurious connectivity caused by common sources (zero-lag correlations).
 
-    REFERENCES: Liu, Z., Fukunaga, M., de Zwart, J. A., & Duyn, J. H. (2010).
-    Large-scale spontaneous fluctuations and correlations in brain electrical
-    activity observed with magnetoencephalography. Neuroimage, 51(1), 102-111.
-    Hipp, J. F., Hawellek, D. J., Corbetta, M., Siegel, M., & Engel,
-    A. K. (2012). Large-scale cortical correlation structure of spontaneous
-    oscillatory activity. Nature neuroscience, 15(6), 884-890.
-    O’Neill, G. C., Barratt, E. L., Hunt, B. A., Tewarie, P. K., & Brookes, M.
-    J. (2015). Measuring electrophysiological connectivity by power envelope
-    correlation: a technical review on MEG methods. Physics in Medicine &
-    Biology, 60(21), R271.
-
     Parameters
     ----------
-    data : numpy 2D matrix
-        MEEG Signal. [n_samples x n_channels].
+    data : numpy.ndarray
+        MEEG Signal. [n_epochs, n_samples, n_channels].
 
     Returns
     -------
-    aec : numpy 2D square matrix
-        aec-based connectivity matrix. [n_channels x n_channels].
+    aec : numpy.ndarray
+        aec-based connectivity matrix. [n_epochs, n_channels, n_channels].
 
     """
-    import tensorflow as tf
-    from tensorflow_probability import stats as tfp_stats
     # Error check
     if type(data) != np.ndarray:
         raise ValueError("Parameter data must be of type numpy.ndarray")
-    if data.shape[0] < data.shape[1]:
-        warnings.warn("Warning: Signal dimensions flipped out. If you have more"
-                      " samples than channels, comment this "
-                      "line")
-        data = data.T
+    # Set to correct dimensions
+    data = check_dimensions(data)
 
     # Variable initialization
-    num_chan = len(data[1])
-    aec = np.empty((num_chan, num_chan))
-    aec[:] = np.nan
+    n_epo = data.shape[0]
+    n_samp = data.shape[1]
+    n_cha = data.shape[2]
+    aec_ort = np.empty((n_epo,n_cha, n_cha))
+    aec_ort[:] = np.nan
     
     # AEC Ort Calculation (CPU orthogonalization is much faster than GPU one)
     signal_ort = orthogonalizate.signal_orthogonalization_cpu(data, data)
+    signal_ort_2 = tf.transpose(tf.reshape(tf.transpose(signal_ort,perm=[0,3,2,1]),
+                                           (n_epo,n_cha*n_cha,
+                                            n_samp)),perm=[0,2,1])
 
-    signal_ort_2 = tf.transpose(tf.reshape(tf.transpose(signal_ort),
-                                           (num_chan*num_chan,
-                                            len(signal_ort))))
     hilb_1 = hilbert(signal_ort_2)
     envelope_1 = tf.math.abs(hilb_1) 
     env = tf.math.log(tf.math.square(envelope_1))
-    aec_tmp = tfp_stats.correlation(env)
-    aec_tmp2 = tf.transpose(
-        tf.reshape(
-            tf.transpose(aec_tmp),
-            (tf.cast(aec_tmp.shape[0]*aec_tmp.shape[0]/num_chan, tf.int32), -1)
-        )
-    )
-    idx = tf.cast(tf.linspace(0, len(aec_tmp2[0])-1, num_chan), tf.int32)
-    aec = tf.gather(aec_tmp2, idx, axis=1).numpy()
 
-    # # Another way of calculating the AEC
-    # for n_chan in range(0,num_chan):
-    #     hilb_1 = hilbert(np.asarray(signal_ort[:,:,n_chan]))
-    #     envelope_1 = tf.math.abs(hilb_1) 
-    #     env = tf.math.log(tf.math.square(envelope_1) )
-    #     aec[:,n_chan] = tf.transpose(tf.slice(tfp_stats.correlation(env),
-    #                                       [0,n_chan],[len(data[0]),1]))
-        
-    # Orthogonalize A regarding B is not the same as orthogonalize B regarding 
-    # A, so we average lower and upper triangular matrices to construct the 
-    # symmetric matrix required for Orthogonalized AEC 
+    # Concurrent calculation for more than one epoch
+    w_threads = []
+    for epoch in env:
+        t = medusa.components.ThreadWithReturnValue(target=__aec_ort_comp_aux,
+                                                    args=(epoch, n_cha, 'gpu',))
+        w_threads.append(t)
+        t.start()
 
-    aec_upper = tf.linalg.band_part(aec, 0, -1)
-    aec_lower = tf.transpose(tf.linalg.band_part(aec, -1, 0))
-    aec_ort = tf.math.divide(tf.math.add(aec_upper, aec_lower), 2)
-    aux = tf.linalg.band_part(aec_ort, 0, -1) - tf.linalg.band_part(
-        aec_ort, 0, 0)
-    aec_ort = tf.math.abs(tf.math.add(aux, tf.transpose(aec_ort)))
+    for epoch_idx, thread in enumerate(w_threads):
+        aec_ort[epoch_idx, :, :] = thread.join()
         
     return aec_ort
 
@@ -177,77 +162,106 @@ def __aec_ort_cpu(data):
     envelope correlation using CPU. This orthogonalized version minimizes the
     spurious connectivity caused by common sources (zero-lag correlations).
 
-    REFERENCES: Liu, Z., Fukunaga, M., de Zwart, J. A., & Duyn, J. H. (2010).
-    Large-scale spontaneous fluctuations and correlations in brain electrical
-    activity observed with magnetoencephalography. Neuroimage, 51(1), 102-111.
-    Hipp, J. F., Hawellek, D. J., Corbetta, M., Siegel, M., & Engel,
-    A. K. (2012). Large-scale cortical correlation structure of spontaneous
-    oscillatory activity. Nature neuroscience, 15(6), 884-890.
-    O’Neill, G. C., Barratt, E. L., Hunt, B. A., Tewarie, P. K., & Brookes, M.
-    J. (2015). Measuring electrophysiological connectivity by power envelope
-    correlation: a technical review on MEG methods. Physics in Medicine &
-    Biology, 60(21), R271.
-
     Parameters
     ----------
-    data : numpy 2D matrix
-        MEEG Signal. [n_samples x n_channels].
+    data : numpy.ndarray
+        MEEG Signal. [n_epochs, n_samples, n_channels].
 
     Returns
     -------
-    aec : numpy 2D square matrix
-        aec-based connectivity matrix. [n_channels x n_channels].
+    aec : numpy.ndarray
+        aec-based connectivity matrix. [n_epochs, n_channels, n_channels].
 
     """
     # Error check
     if type(data) != np.ndarray:
         raise ValueError("Parameter data must be of type numpy.ndarray")
-    if data.shape[0] < data.shape[1]:
-        warnings.warn("Warning: Signal dimensions flipped out. If you have more"
-                      " samples than channels, comment this "
-                      "line")
-        data = data.T
+
+    # Set to correct dimensions
+    data = check_dimensions(data)
 
     # Variable initialization
-    n_cha = len(data[1])
-    aec = np.empty((n_cha, n_cha))
-    aec[:] = np.nan
+    n_epo = data.shape[0]
+    n_samp = data.shape[1]
+    n_cha = data.shape[2]
+    aec_ort = np.empty((n_epo, n_cha, n_cha))
+    aec_ort[:] = np.nan
 
     # AEC Ort Calculation
     signal_ort = orthogonalizate.signal_orthogonalization_cpu(data, data)
+    signal_ort_2 = np.transpose(np.reshape(np.transpose(signal_ort, (0, 3, 2, 1)),
+                            (n_epo, n_cha * n_cha, n_samp)),(0,2,1))
 
-    # 1st method of calculating the Ort AEC. Faster
-    signal_ort_2 = np.transpose(np.reshape(np.transpose(signal_ort),
-                                           (n_cha*n_cha,
-                                            len(signal_ort))))
     hilb_1 = hilbert(signal_ort_2)
     envelope_1 = np.abs(hilb_1)
     env = np.log(np.square(envelope_1))
-    aec_tmp = np.corrcoef(env, rowvar=False)
-    aec_tmp2 = np.transpose(
-        np.reshape(
-            np.transpose(aec_tmp),
-            (int(aec_tmp.shape[0]*aec_tmp.shape[0]/n_cha), -1)
-        )
-    )
-    idx = np.linspace(0, aec_tmp2.shape[1]-1, n_cha).astype(np.int32)
-    aec = aec_tmp2[:, idx]
 
-    # 2nd method of calculating the Ort AEC
-    # for cha in range(n_cha):
-    #     hilb = hilbert(signal_ort[:, :, cha])
-    #     env = np.log(np.square(np.abs(hilb)))
-    #     aec[:, cha] = np.corrcoef(env.T)[:, cha]
+    # Concurrent calculation for more than one epoch
+    w_threads = []
+    for epoch in env:
+        t = medusa.components.ThreadWithReturnValue(target=__aec_ort_comp_aux,
+                                                    args=(epoch,n_cha,'cpu',))
+        w_threads.append(t)
+        t.start()
 
-    # Orthogonalize A regarding B is not the same as orthogonalize B regarding
-    # A, so we average lower and upper triangular matrices to construct the
-    # symmetric matrix required for Orthogonalized AEC
-    aec_upper = np.triu(np.squeeze(aec))
-    aec_lower = np.transpose(np.tril(np.squeeze(aec)))
-    aec_ort = (aec_upper + aec_lower) / 2
-    aec_ort = abs(np.triu(aec_ort, 1) + np.transpose(aec_ort))
+    for epoch_idx, thread in enumerate(w_threads):
+        aec_ort[epoch_idx,:,:] = thread.join()
 
     return aec_ort
+
+def __aec_ort_comp_aux(env, n_cha, ctype='cpu'):
+    """
+    Auxiliary method that implements a function to compute the orthogonalized AEC.
+    Parameters
+    ----------
+    env: numpy.ndarray
+        Array with signal envelope. [n_epochs, n_samples, n_channels x n_channels].
+    type: str
+        Calculation type: 'cpu' or 'gpu'.
+    Returns
+    -------
+    aec_ort: numpy.ndarray
+        AEC orthogonalized connectivity matrix. [n_channels, n_channels].
+    """
+    # Note: Orthogonalize A regarding B is not the same as orthogonalize B regarding
+    # A, so we average lower and upper triangular matrices to construct the
+    # symmetric matrix required for Orthogonalized AEC
+
+    if ctype == 'cpu':
+        aec_tmp = np.corrcoef(env, rowvar=False)
+        aec_tmp2 = np.transpose(
+            np.reshape(
+                np.transpose(aec_tmp),
+                (int(aec_tmp.shape[0] * aec_tmp.shape[0] / n_cha), -1)
+            )
+        )
+        idx = np.linspace(0, aec_tmp2.shape[1] - 1, n_cha).astype(np.int32)
+        aec = aec_tmp2[:, idx]
+        aec_upper = np.triu(np.squeeze(aec))
+        aec_lower = np.transpose(np.tril(np.squeeze(aec)))
+        aec_ort = (aec_upper + aec_lower) / 2
+        aec_ort = abs(np.triu(aec_ort, 1) + np.transpose(aec_ort))
+        return aec_ort
+
+    elif ctype == 'gpu':
+        aec_tmp = tfp_stats.correlation(env)
+        aec_tmp2 = tf.transpose(
+            tf.reshape(
+                tf.transpose(aec_tmp),
+                (tf.cast(aec_tmp.shape[0] * aec_tmp.shape[0] / n_cha,
+                         tf.int32), -1)
+            )
+        )
+        idx = tf.cast(tf.linspace(0, len(aec_tmp2[0]) - 1, n_cha), tf.int32)
+        aec = tf.gather(aec_tmp2, idx, axis=1).numpy()
+        aec_upper = tf.linalg.band_part(aec, 0, -1)
+        aec_lower = tf.transpose(tf.linalg.band_part(aec, -1, 0))
+        aec_ort = tf.math.divide(tf.math.add(aec_upper, aec_lower), 2)
+        aux = tf.linalg.band_part(aec_ort, 0, -1) - tf.linalg.band_part(
+            aec_ort, 0, 0)
+        aec_ort = tf.math.abs(tf.math.add(aux, tf.transpose(aec_ort)))
+        return aec_ort
+
 
 
 def aec(data, ort=True):
