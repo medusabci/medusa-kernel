0a6b4aa4a0902ed188dcb6c3665b02653e5e6ad4
esantamariavazquez <esantamariavazquez@gmail.com>
2022-12-07 13:43:07 +0100

    Package medusa.bci.remote has been eliminated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='medusa-kernel',
     packages=find_packages(),
-    version='1.1.1',
+    version='1.1.2',
     keywords=['Signal', 'Biosignal', 'EEG', 'BCI'],
     url='https://medusabci.com/',
     author='Eduardo Santamaría-Vázquez, '
